7b3c72cf763f22c7fd82e62ef2e07b964235f794
gadiaz02 <gadiaz02@ucm.es>
2025-02-09 12:13:04 +0100

    SnailSortOfWorking

--- src/StateMachine/snailState.js
+++ src/StateMachine/snailState.js
@@ -17,14 +17,21 @@ export class SnailState extends State{
         this.down = false;
         this.topFallingSpeed = 200;
 
+        this.isStuck = false;
+        this.shouldFall = true;
+
     }
 
     stickToSurface() {
 
         console.log(this.left, this.right, this.top);
+        this.isStuck = true;
+        this.player.body.setOffset(11.5,14.5);
 
         if (this.left) {
             //izquierda
+            this.player.setPosition(this.player.x + 1, this.player.y);
+            this.player.body.setOffset(9,12);
             this.player.setFlipX(false);
             this.player.body.setAllowGravity(false);
             console.log("IZQUIERDAAA");
@@ -33,6 +40,8 @@ export class SnailState extends State{
             this.player.body.setVelocityY(0);
         } else if (this.right) {
             //derecha
+            this.player.setPosition(this.player.x - 2, this.player.y);
+            this.player.body.setOffset(15,12);
             this.player.setFlipX(true);
             this.player.body.setAllowGravity(false);
             console.log("DERECHAAAA");
@@ -41,6 +50,8 @@ export class SnailState extends State{
             this.player.body.setVelocityY(0);
         } else if (this.top){
             //techo
+            this.player.setPosition(this.player.x, this.player.y + 6);
+            this.player.body.setOffset(11.5,8);
             this.player.body.setGravityY(0);
             this.player.body.setAllowGravity(false);
             console.log("TECHOOO");
@@ -68,10 +79,11 @@ export class SnailState extends State{
         this.left = this.left || this.player.body.blocked.left;
         this.right = this.right || this.player.body.blocked.right;
         this.top = this.top || this.player.body.blocked.up;
+        this.shouldFall = !(this.left || this.right || this.top || this.player.body.onFloor());
 
-        if(this.left || this.right || this.top || this.player.body.onFloor())
+        if((this.left || this.right || this.top || this.player.body.onFloor()) && !this.isStuck)
             this.stickToSurface();
-        else if (this.topFallingSpeed > this.player.body.velocity.y){
+        else if (this.topFallingSpeed > this.player.body.velocity.y && this.shouldFall){
             this.player.body.setVelocityY(this.player.body.velocity.y + 1 * dt);
             if (this.topFallingSpeed < this.player.body.velocity.y)
                 this.player.body.setVelocityY(this.topFallingSpeed);
