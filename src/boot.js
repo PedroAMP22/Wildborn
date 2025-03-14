64671acf63669ad9454833cc6f7e2c7cd3b685f5
PedroAMP <a2017martinp@gmail.com>
2025-03-14 11:24:18 +0100

    Menu Title

--- src/boot.js
+++ src/boot.js
@@ -279,7 +279,7 @@ export default class Boot extends Phaser.Scene {
   }
 
   create() {
-    this.scene.start('screen1_1');
+    this.scene.start('screenMenu');
 
   }
 }
--- src/menus/MenuScreen.js
+++ src/menus/MenuScreen.js
@@ -6,19 +6,32 @@ export default class MenuScene extends ScreenBase {
     }
 
     preload() {
+        this.load.image('wildBornTitle', '../../assets/tilemaps/menuImages/MenuTitle.png');
     }
 
     create() {
         super.create()
 
+        this.objectsLayer.objects.forEach(({ name, x, y, width, height }) => {
+            if(name === "cameraFocus")
+                this.cameraFocus = {x,y};
+            
+        });
+
         //background image
-        this.backgroundImage = this.add.image(0, 0, "MountainBG").setOrigin(0, 0);
+        this.backgroundImage = this.add.image(0, 0, "MountainBG").setOrigin(0.1);
         this.backgroundImage.setDepth(-10);
         this.backgroundImage.setScrollFactor(0);
 
         this.textures.get("MountainBG").setFilter(Phaser.Textures.FilterMode.NEAREST);
         
-        this.input.keyboard.enabled = false;
+        //TODO This will be set at false when the menu is done
+        this.input.keyboard.enabled = true;
+
+        this.cameras.main.setZoom(0.8);
+
+        
+
 
         this.createMenu();
 
@@ -26,23 +39,13 @@ export default class MenuScene extends ScreenBase {
 
     createMenu() {
         // Menu Background Panel
-        this.menuPanel = this.add.image(this.scale.width / 2, this.scale.height / 2, "menuBackground")
-            .setOrigin(0.5)
-            .setDepth(100)
-            .setAlpha(0.9);
+        const titleImage = this.add.image(250, 130, 'wildBornTitle');
+        titleImage.setOrigin(0.5); 
+        titleImage.setDepth(10)
+        titleImage.setScale(0.20)
 
-        // Button A
-        this.buttonPlay = this.add.image(this.scale.width / 2, this.scale.height / 2 - 50, "buttonPlay")
-            .setOrigin(0.5)
-            .setDepth(101)
-            .setInteractive()
-            .on('pointerdown', () => this.play());
-
-        // Button B
-        this.buttonSelector = this.add.image(this.scale.width / 2, this.scale.height / 2 + 50, "buttonSelector")
-            .setOrigin(0.5)
-            .setDepth(101)
-            .setInteractive()
+        this.cameras.main.startFollow(this.cameraFocus,true,0.1, 50)
+        
     }
 
     play(){
