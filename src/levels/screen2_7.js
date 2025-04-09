0d53875e861e32ea2e7f77be66b8aa9273bc71cb
gadiaz02 <gadiaz02@ucm.es>
2025-04-09 14:27:29 +0200

    change 2_7

--- src/levels/screen2_7.js
+++ src/levels/screen2_7.js
@@ -53,7 +53,7 @@ export default class Screen2_7 extends ScreenBase {
 
 
         //background image
-        this.backgroundImage = this.add.image(0, 0, "ForestBG2").setOrigin(0, 0);
+        this.backgroundImage = this.add.image(0, 0, "MountainBG").setOrigin(0, 0);
         this.backgroundImage.setDepth(-10);
         this.backgroundImage.setScrollFactor(0);
              
